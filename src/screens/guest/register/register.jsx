730b03c2b8782b53a8bad10ee5b514a4633f6d3f
Seyi_Coded <opadonuseyi01@gmail.com>
2022-11-09 00:24:17 +0100

    sign up adjustment

--- src/screens/guest/register/register.jsx
+++ src/screens/guest/register/register.jsx
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import COLOR from '../../../constant/color'
 import { Button, TextInput } from 'react-native-paper'
 import ROUTE from '../../../constant/route'
